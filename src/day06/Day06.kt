ca416318b92f0f5adfe83c761502b0a487dc749b
iulianp <iulianpopescu1394@gmail.com>
2022-12-12 03:40:01 +0200

    Update AOC day 6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -6,24 +6,18 @@ private const val DAY = "06"
 private const val DAY_TEST = "day${DAY}/Day${DAY}_test"
 private const val DAY_INPUT = "day${DAY}/Day${DAY}"
 fun main() {
-    fun findUniqueForLength(code: String, length: Int): Int {
-        val queue = ArrayDeque<Char>()
-        queue.addAll(code.take(length).toList())
-        if (queue.containsUniqueItems())
-            return length
+    fun findUniqueSubstringIndex(code: String, length: Int) = code
+        .windowed(length)
+        .mapIndexed { index, substring ->
+            // reconstruct index from the original string before splitting into slices
+            index + length to substring.containsUniqueChars()
+        }.find {
+            it.second
+        }?.first ?: throw IllegalStateException("No index found to satisfy condition")
 
-        for (i in length..code.lastIndex) {
-            queue.removeFirst()
-            queue.add(code[i])
-            if (queue.containsUniqueItems())
-                return i + 1
-        }
-        throw IllegalStateException("No index found to satisfy condition")
-    }
+    fun part1(input: List<String>) = findUniqueSubstringIndex(input.first(), length = 4)
 
-    fun part1(input: List<String>) = findUniqueForLength(input.first(), 4)
-
-    fun part2(input: List<String>) = findUniqueForLength(input.first(), 14)
+    fun part2(input: List<String>) = findUniqueSubstringIndex(input.first(), length = 14)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput(DAY_TEST)
@@ -31,11 +25,11 @@ fun main() {
     check(part2(testInput) == 19)
 
     val input = readInput(DAY_INPUT)
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) // 1848
+    println(part2(input)) // 2308
 }
 
-private fun ArrayDeque<Char>.containsUniqueItems(): Boolean {
+private fun String.containsUniqueChars(): Boolean {
     val itemsSet = HashSet<Char>()
     return this.all { item ->
         !itemsSet.contains(item).also {
