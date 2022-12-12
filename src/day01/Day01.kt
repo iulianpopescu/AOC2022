38c70ce7ee96189720db36491c7436c7d35f29ec
iulianp <iulianpopescu1394@gmail.com>
2022-12-12 03:53:09 +0200

    Update AOC day 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,36 +1,33 @@
 package day01
 
-import readInput
+import readFile
 
 private const val DAY = "01"
 private const val DAY_TEST = "day${DAY}/Day${DAY}_test"
 private const val DAY_INPUT = "day${DAY}/Day${DAY}"
 fun main() {
-    fun getCaloriesList(input: List<String>): List<Int> {
-        var collector = 0
-        val calories = mutableListOf<Int>()
-        input.forEach { calorieString ->
-            if (calorieString == "") {
-                calories.add(collector)
-                collector = 0
-            } else {
-                collector += calorieString.toInt()
-            }
+    fun sumTopCalories(input: String, limit: Int = 1) = input
+        .split("\n\n")
+        .asSequence()
+        .map { backpack ->
+            val calories = backpack.split("\n")
+            calories.map { it.toInt() }
         }
-        calories.add(collector)
-        return calories.sortedDescending()
-    }
+        .map { it.sum() }
+        .sortedDescending()
+        .take(limit)
+        .sum()
 
-    fun part1(input: List<String>) = getCaloriesList(input).first()
+    fun part1(input: String) = sumTopCalories(input)
 
-    fun part2(input: List<String>) = getCaloriesList(input).take(3).sum()
+    fun part2(input: String) = sumTopCalories(input, 3)
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput(DAY_TEST)
+    val testInput = readFile(DAY_TEST)
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
-    val input = readInput(DAY_INPUT)
-    println(part1(input))
-    println(part2(input))
+    val input = readFile(DAY_INPUT)
+    println(part1(input)) // 73211
+    println(part2(input)) // 213958
 }
